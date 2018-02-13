978cbb91b7c33c0326804c10a12730ab0ce683af
Shane Tidwell <shane.tidwell21@gmail.com>
2018-02-12 18:27:23 -0700

    completed project

--- src/Stage 5/components/EmployeeEditor.js
+++ src/Stage 5/components/EmployeeEditor.js
@@ -8,15 +8,36 @@ class EmployeeEditor extends Component {
       originalEmployee: null,
       notModified: true
     };
+    //this.handleChange = this.handleChange.bind(this);
+    this.save = this.save.bind(this);
+    this.cancel = this.cancel.bind(this);
+  }
+  componentWillReceiveProps(props){
+    this.setState({employee: Object.assign({}, props.selected), originalEmployee: props.selected, notModified:true})
+  }
+ 
+  handleChange(prop,val){
+    if(this.state.notModified){
+      this.setState({notModified:false})
+    }
+    var employeeCopy = Object.assign({}, this.state.employee);
+    employeeCopy[prop] = val;
+    this.setState({employee: employeeCopy})
   }
 
-  // componentWillReceiveProps
 
-  // handleChange
+  save(){
+    this.state.originalEmployee.updateName(this.state.employee.name);
+    this.state.originalEmployee.updatePhone(this.state.employee.phone);
+    this.state.originalEmployee.updateTitle(this.state.employee.title);
+    this.setState({notModified: true});
+    this.props.refreshList();
+  }
 
-  // save
-
-  // cancel
+  cancel(){
+    var employeeCopy = Object.assign({},this.state.originalEmployee);
+    this.setState({employee: employeeCopy, notModified: true});
+  }
   
   render() {
     return (
